63fb1d269deac52015dbf8cc45a8b025f25721f5
SteakTheStake <gabecorr25@gmail.com>
2024-08-06 09:41:21 -0700

    p3

--- gup.py
+++ gup.py
@@ -6,7 +6,7 @@ gup = Flask(__name__)
 # main route
 @gup.route('/')
 def gup_login():
-    return render_template('index.html')
+    return render_template('landing.html')
 
 
 if __name__ == "__main__":
